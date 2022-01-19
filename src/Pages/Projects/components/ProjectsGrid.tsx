7e4dc45b244423eb736104ad2220bf835c80f982
ViktorSheverdin <viktor.sheverdin@elphasecure.com>
2022-01-18 17:58:53 -0800

    fixing grid bug

--- src/Pages/Projects/components/ProjectsGrid.tsx
+++ src/Pages/Projects/components/ProjectsGrid.tsx
@@ -14,10 +14,17 @@ const ProjectsGrid = ({ filters }) => {
         }, []);
 
   return (
-    <Grid container spacing={'2.8rem'} sx={{ width: '100%' }}>
+    <Grid
+      container
+      direction={'row'}
+      spacing={'2.8rem'}
+      // wrap='nowrap'
+      sx={{ width: '100%' }}
+    >
       {filteredProjects.map((project, i) => {
         return (
           <Grid key={i} item xs={'auto'}>
+            {/* <Grid key={i} item xs={4}> */}
             <ProjectContent project={project} />
           </Grid>
         );
