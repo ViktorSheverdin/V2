fc26c0b5c606b8ef73c48a47a1721f01e04f207f
ViktorSheverdin <viktor.sheverdin@elphasecure.com>
2022-03-08 18:06:29 -0800

    projedct modal

--- src/Pages/Projects/components/ProjectContent.tsx
+++ src/Pages/Projects/components/ProjectContent.tsx
@@ -1,72 +1,107 @@
 import { useState } from 'react';
-import { Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import theme from '../../../config/material-theme';
+import ProjectDetailsModal from '../../../components/Modals/ProjectDetailsModal';
 
-const ProjectContent = ({ project }) => {
-  const [hover, setHover] = useState(false);
-
+const HoverDisplay = ({
+  project,
+  openProjectDetailsModal,
+  setOpenProjectDetailsModal,
+}) => {
   return (
-    <Stack
-      onMouseOver={() => setHover(true)}
-      onMouseOut={() => setHover(false)}
-      sx={{
-        position: 'relative',
-        width: '100%',
-        margin: 0,
-        '&:hover': {
-          cursor: 'pointer',
-        },
-      }}
-    >
-      <img
-        src={project.img}
-        alt='img'
-        style={{
-          backgroundRepeat: 'no-repeat',
+    <>
+      <Stack
+        direction='column'
+        sx={{
+          position: 'absolute',
           width: '100%',
+          height: '100%',
+          backgroundImage: `linear-gradient(90deg,${theme.palette.primary.main},${theme.palette.secondary.main})`,
+          alignContent: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
         }}
-      />
-      {hover && (
+      >
+        <Typography
+          sx={{ position: 'relative', fontSize: '2rem', fontWeight: '700' }}
+        >
+          {project.name}
+        </Typography>
         <Stack
-          direction='column'
+          direction='row'
           sx={{
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            backgroundImage: `linear-gradient(90deg,${theme.palette.primary.main},${theme.palette.secondary.main})`,
+            position: 'relative',
             alignContent: 'center',
             justifyContent: 'center',
             textAlign: 'center',
           }}
         >
-          <Typography sx={{ fontSize: '2rem', fontWeight: '700' }}>
-            {project.name}
-          </Typography>
-          <Stack
-            direction='row'
-            sx={{
-              alignContent: 'center',
-              justifyContent: 'center',
-              textAlign: 'center',
-            }}
-          >
-            {project.technology.map((tech, i) => {
-              return (
-                <Typography
-                  sx={{
-                    fontSize: '1.6rem',
-                    fontWeight: '400',
-                    marginTop: '0.8rem',
-                  }}
-                >
-                  {tech} {project.technology.length - i > 1 ? '/ ' : ''}{' '}
-                </Typography>
-              );
-            })}
-          </Stack>
+          {project.technology.map((tech, i) => {
+            return (
+              <Typography
+                sx={{
+                  position: 'relative',
+                  fontSize: '1.6rem',
+                  fontWeight: '400',
+                  marginTop: '0.8rem',
+                }}
+              >
+                {tech} {project.technology.length - i > 1 ? '/ ' : ''}{' '}
+              </Typography>
+            );
+          })}
         </Stack>
-      )}
-    </Stack>
+      </Stack>
+    </>
+  );
+};
+
+const ProjectContent = ({ project }) => {
+  const [parentHover, setParentHover] = useState(false);
+  const [openProjectDetailsModal, setOpenProjectDetailsModal] = useState(false);
+  console.log(parentHover);
+  return (
+    <>
+      <Stack
+        onMouseOver={(e) => {
+          e.preventDefault();
+          setParentHover(true);
+        }}
+        onMouseOut={(e) => {
+          e.preventDefault();
+          setParentHover(false);
+        }}
+        onClick={() => setOpenProjectDetailsModal(true)}
+        sx={{
+          position: 'relative',
+          width: '100%',
+          margin: 0,
+          '&:hover': {
+            cursor: 'pointer',
+          },
+        }}
+      >
+        <img
+          src={project.img}
+          alt='img'
+          style={{
+            backgroundRepeat: 'no-repeat',
+            width: '100%',
+          }}
+        />
+        {parentHover && (
+          <HoverDisplay
+            project={project}
+            openProjectDetailsModal={openProjectDetailsModal}
+            setOpenProjectDetailsModal={setOpenProjectDetailsModal}
+          />
+        )}
+      </Stack>
+      <ProjectDetailsModal
+        open={openProjectDetailsModal}
+        onClose={() => setOpenProjectDetailsModal(false)}
+      />
+    </>
   );
 };
 
