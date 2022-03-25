bedd4e4956cc39e431ef1f9243bbf757de16db0a
Viktor Sheverdin <32000453+ViktorSheverdin@users.noreply.github.com>
2022-03-25 12:51:01 -0700

    modal implemented, flickering fixed (#6)
    Co-authored-by: ViktorSheverdin <viktor.sheverdin@elphasecure.com>

--- src/Pages/Projects/components/ProjectContent.tsx
+++ src/Pages/Projects/components/ProjectContent.tsx
@@ -1,105 +1,90 @@
 import { useState } from 'react';
-import { Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import theme from '../../../config/material-theme';
 import ProjectDetailsModal from '../../../components/Modals/ProjectDetailsModal';
 
-const HoverDisplay = ({
-  project,
-  openProjectDetailsModal,
-  setOpenProjectDetailsModal,
-}) => {
+const HoverDisplay = ({ project }) => {
   return (
-    <>
+    <Stack
+      direction='column'
+      sx={{
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundImage: `linear-gradient(90deg,${theme.palette.primary.main},${theme.palette.secondary.main})`,
+        alignContent: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        zIndex: 999,
+      }}
+    >
+      <Typography
+        sx={{ position: 'relative', fontSize: '2rem', fontWeight: '700' }}
+      >
+        {project.name}
+      </Typography>
       <Stack
-        direction='column'
+        direction='row'
         sx={{
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          backgroundImage: `linear-gradient(90deg,${theme.palette.primary.main},${theme.palette.secondary.main})`,
+          position: 'relative',
           alignContent: 'center',
           justifyContent: 'center',
           textAlign: 'center',
         }}
       >
-        <Typography
-          sx={{ position: 'relative', fontSize: '2rem', fontWeight: '700' }}
-        >
-          {project.name}
-        </Typography>
-        <Stack
-          direction='row'
-          sx={{
-            position: 'relative',
-            alignContent: 'center',
-            justifyContent: 'center',
-            textAlign: 'center',
-          }}
-        >
-          {project.technology.map((tech, i) => {
-            return (
-              <Typography
-                sx={{
-                  position: 'relative',
-                  fontSize: '1.6rem',
-                  fontWeight: '400',
-                  marginTop: '0.8rem',
-                }}
-              >
-                {tech} {project.technology.length - i > 1 ? '/ ' : ''}{' '}
-              </Typography>
-            );
-          })}
-        </Stack>
+        {project.technologies.map((tech, i) => {
+          return (
+            <Typography
+              sx={{
+                position: 'relative',
+                fontSize: '1.6rem',
+                fontWeight: '400',
+                marginTop: '0.8rem',
+              }}
+            >
+              {tech} {project.technologies.length - i > 1 ? '/ ' : ''}{' '}
+            </Typography>
+          );
+        })}
       </Stack>
-    </>
+    </Stack>
   );
 };
 
 const ProjectContent = ({ project }) => {
   const [parentHover, setParentHover] = useState(false);
   const [openProjectDetailsModal, setOpenProjectDetailsModal] = useState(false);
-  console.log(parentHover);
   return (
     <>
-      <Stack
-        onMouseOver={(e) => {
-          e.preventDefault();
+      <Box
+        onMouseEnter={() => {
           setParentHover(true);
         }}
-        onMouseOut={(e) => {
-          e.preventDefault();
+        onMouseLeave={() => {
           setParentHover(false);
         }}
-        onClick={() => setOpenProjectDetailsModal(true)}
+        onClick={(e) => {
+          e.preventDefault();
+          setOpenProjectDetailsModal(true);
+        }}
         sx={{
           position: 'relative',
           width: '100%',
+          height: '100%',
           margin: 0,
-          '&:hover': {
-            cursor: 'pointer',
-          },
+          cursor: parentHover ? 'pointer' : '',
         }}
       >
-        <img
-          src={project.img}
-          alt='img'
-          style={{
-            backgroundRepeat: 'no-repeat',
-            width: '100%',
-          }}
-        />
-        {parentHover && (
-          <HoverDisplay
-            project={project}
-            openProjectDetailsModal={openProjectDetailsModal}
-            setOpenProjectDetailsModal={setOpenProjectDetailsModal}
-          />
-        )}
-      </Stack>
+        {parentHover && <HoverDisplay project={project} />}
+
+        <Box sx={{ height: '100%', width: '100%', position: 'relative' }}>
+          <img src={project.img} alt='img' height='100%' width='100%' />
+        </Box>
+      </Box>
       <ProjectDetailsModal
         open={openProjectDetailsModal}
         onClose={() => setOpenProjectDetailsModal(false)}
+        project={project}
       />
     </>
   );
--- src/components/Modals/ProjectDetailsModal.tsx
+++ src/components/Modals/ProjectDetailsModal.tsx
@@ -1,12 +1,137 @@
-import { Box, Dialog, DialogContent } from '@mui/material';
-import React from 'react';
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Link,
+  Stack,
+  Typography,
+} from '@mui/material';
 
-const ProjectDetailsModal = ({ open, onClose }) => {
+const ProjectDetailsModal = ({ open, onClose, project }) => {
+  console.log(project);
   return (
-    <Dialog open={open} onClose={onClose} fullWidth maxWidth='xs' scroll='body'>
+    <Dialog
+      open={open}
+      onClose={onClose}
+      fullWidth
+      // maxWidth='sm'
+      scroll='body'
+    >
       <DialogContent>
-        <Box>Modal</Box>
+        <Stack direction='column'>
+          <Typography
+            sx={{
+              fontWeight: '700',
+              fontSize: '2.75rem',
+              lineHeight: '2rem',
+              color: 'primary.600',
+            }}
+          >
+            {project.name}
+          </Typography>
+          <Typography
+            sx={{
+              fontWeight: '400',
+              fontSize: '1.6rem',
+              lineHeight: '1.4rem',
+              color: 'primary.400',
+              marginTop: '1rem',
+            }}
+          >
+            {project.shortDescription}
+          </Typography>
+          <Box sx={{ width: '100%', height: 'auto', marginTop: '2rem' }}>
+            <img src={project.img} alt='img' height='100%' width='100%' />
+          </Box>
+          <Typography
+            sx={{
+              fontWeight: '500',
+              fontSize: '2.5rem',
+              lineHeight: '2.5rem',
+              color: 'primary.600',
+              marginTop: '2rem',
+            }}
+          >
+            About
+          </Typography>
+          <Typography
+            sx={{
+              fontWeight: '400',
+              fontSize: '1.4rem',
+              lineHeight: '1.4rem',
+              color: 'primary.400',
+              marginTop: '1rem',
+            }}
+          >
+            {project.description}
+          </Typography>
+          <Typography
+            sx={{
+              fontWeight: '500',
+              fontSize: '2.5rem',
+              lineHeight: '2.5rem',
+              color: 'primary.600',
+              marginTop: '1rem',
+            }}
+          >
+            Technologies
+          </Typography>
+          <Stack direction={'row'} sx={{ marginTop: '1rem' }}>
+            {project.technologies.map((tech) => {
+              return <Typography>{tech}</Typography>;
+            })}
+          </Stack>
+          <Typography
+            sx={{
+              fontWeight: '500',
+              fontSize: '2.5rem',
+              lineHeight: '2.5rem',
+              color: 'primary.600',
+              marginTop: '1rem',
+            }}
+          >
+            Github
+          </Typography>
+          <Link
+            sx={{
+              fontWeight: '400',
+              fontSize: '1.4rem',
+              lineHeight: '1.4rem',
+              color: 'primary.400',
+              marginTop: '1rem',
+              '&:hover': {
+                cursor: 'pointer',
+              },
+            }}
+          >
+            {project.github}
+          </Link>
+        </Stack>
       </DialogContent>
+      <DialogActions>
+        <Button
+          variant={'outlined'}
+          sx={{
+            width: '100%',
+          }}
+          href={project.demoURL}
+          target='_blank'
+        >
+          {' '}
+          <Typography
+            sx={{
+              fontWeight: '500',
+              fontSize: '1.5rem',
+              lineHeight: '1.5rem',
+              color: 'primary.600',
+            }}
+          >
+            Open Project
+          </Typography>
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
